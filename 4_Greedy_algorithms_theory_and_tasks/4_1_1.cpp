99768a078a3d21309dd43441a6064f8077cc1ff8
Osipov-mo <85345205+Osipov-mo@users.noreply.github.com>
2021-06-04 14:59:31 +0300

    Update 4_1_1.cpp

--- 4_Greedy_algorithms_theory_and_tasks/4_1_1.cpp
+++ 4_Greedy_algorithms_theory_and_tasks/4_1_1.cpp
@@ -1,55 +1,59 @@
 #include <iostream>
-#include <ios>
 #include <algorithm>
-#include <cinttypes>
-#include <cassert>
 #include <vector>
-
+#include <queue>
+ 
 using namespace std;
-
-struct Item final {
-    int weight;
-    int value;
-    };
-
-double get_max_knapsack_value(int capacity, vector <Item> items) {
-    sort(items.begin(), items.end(), [](const Item &lhs, const Item &rhs) {
-        return static_cast<int64_t>(lhs.weight) * rhs.value <
-        static_cast<int64_t>(rhs.weight) * lhs.value;
-        });
-
-    double value = 0.0;
+ 
+bool comp(vector<int>&f,vector<int>&s) {
+    if(f[0] == s[0])return (f[1] < s[1]);
+    else return (f[0] < s[0]); 
+}
+ 
+int main() 
+{
+    int n;
+    cin >> n;
     
-    for(auto &item:items) {
-        if (capacity > item.weight) {
+    vector<vector<int>>v(n,vector<int>(2));
+    
+    for(int i = 0; i<n; ++i) cin >> v[i][0] >> v[i][1];
+    
+    vector<vector<int>>temp;
+    
+    for(int i = 0;i<n;++i) {
+        temp.push_back({v[i][0],0,i});
+        temp.push_back({v[i][1],1,i});
+    }
+    
+    sort(temp.begin(),temp.end(),comp);
+    
+    vector<int>ok(n);
+    vector<int>ans;
+    
+    vector<int>nul(n);
+    
+    for(int i = 0;i<temp.size();++i) {
         
-            capacity -= item.weight;
-            value += item.value;
-        } else {
-            value += item.value * (static_cast <double>(capacity) / item.weight);
-            break;
+        int f = temp[i][0];
+        int s = temp[i][1];
+        int t = temp[i][2];
+        
+        //cout << f << ' ' << s  << ' ' << ok[t] << "\n";
+        
+        if(s == 0)ok[t] = 1;
+        else {
+            if(ok[t]) {
+                
+                ok = nul;
+                ans.push_back(f);
+            }
         }
     }
-
-    return value;
-}
-
-int main(void) {
     
-    int number_of_items;
-    int knapsack_capacity;
+    cout << ans.size() << "\n";
     
-    cin >> number_of_items >> knapsack_capacity;
-    
-    vector <Item> items(number_of_items);
-    
-    for(int i = 0; i < number_of_items; i++) {
-        cin >> items[i].value >> items[i].weight;
-    }
-
-    double max_knapsack_value = get_max_knapsack_value(knapsack_capacity, move(items));
-
-    cout << max_knapsack_value;
+    for(int i = 0;i<ans.size();++i)cout << ans[i] << ' ';
     
     return 0;
 }
